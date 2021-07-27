35a1fb6d7ba16b3a9e73e2039d619476f185938b
octaviofitz <octaviofitz@gmail.com>
2021-07-27 02:39:21 -0300

    cambios header y agrego detalle de producto parametrizado

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,10 +1,17 @@
 const categorias= require ('../data/categories_db')
+const productos= require('../data/products_db')
 
 module.exports= {
     add: (req,res) => {
         return res.render('productAdd',{
             title: "Crafty2.0",
             categorias,
+        })
+    },
+    detail: (req,res) => {
+        let producto= productos.find(producto => producto.id === +req.params.id)
+        return res.render('productDetail',{
+            producto
         })
     },
 }
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-const {add}= require('../controllers/productsController')
+const {add, detail}= require('../controllers/productsController')
 
 /* GET users listing. */
 router.get('/add',add)
+router.get('/detail/:id',detail)
+
 
 
 module.exports = router;
